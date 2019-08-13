76d8d9f6918f66087b84e552dc810732e1aea599
Akshay Deshpande <Akshay14469@Mastek.Com>
2019-08-13 17:36:11 +0530

    Changed response json format

--- NHS.Api/riak_crud/__init__.py
+++ NHS.Api/riak_crud/__init__.py
@@ -2,10 +2,10 @@ import riak
 
 
 def create_client():
-    # client = riak.RiakClient(host="18.217.190.19")
-    client = riak.RiakClient(host="18.188.197.228")
+    client = riak.RiakClient(host="3.16.26.40")
     return client
 
 
 riak_client = create_client()
-bucket_name = "trust_performance"
+trust_bucket_name = "trust_performance"
+region_bucket_name = "region_performance"
--- NHS.Api/riak_crud/views.py
+++ NHS.Api/riak_crud/views.py
@@ -2,34 +2,13 @@
 from __future__ import unicode_literals
 
 from django.http import JsonResponse, HttpResponse
+from django.views.decorators.http import require_POST
 import riak_crud
 
 # This Hardcoded data is to be removed.
 # Schema of DATA is not final yet, It might change.
-# DATA = {
-#    "E1": {
-#        "avg_gap": 10,
-#        "patient_count": 100,
-#    },
-#    "E2": {
-#        "avg_gap": 5,
-#        "patient_count": 100,
-#    },
-#    "E3": {
-#        "avg_gap": 8,
-#        "patient_count": 100,
-#    },
-#    "E4": {
-#        "avg_gap": 8,
-#        "patient_count": 100,
-#    },
-#    "E5": {
-#        "avg_gap": 6,
-#        "patient_count": 100,
-#    }
-# }
-
-DATA = {
+# Repetition of CRUD Logic is to be removed.
+TrustData = {
     "E1": 20,
     "E2": 100,
     "E3": 25,
@@ -37,31 +16,112 @@ DATA = {
     "E5": 56,
 }
 
+RegionData = {
+    "E1": 20,
+    "E2": 30,
+    "E3": 40,
+    "E4": 50,
+    "E5": 60
+}
 
-def search(request, key):
-    bucket = riak_crud.riak_client.bucket(riak_crud.bucket_name)
-    user_obj = bucket.get(key)
-    if not user_obj.data:
-        return HttpResponse("No Data with this key is present in this bucket")
-    return JsonResponse(user_obj.data)
+RegionTrustMap = {
+    "R0": ["RR1", "RR2"],
+    "R1": ["RR3", "RR4", "RR5"],
+    "R2": ["RR6", "RR7"],
+    "R3": ["RR8", "RR9", "RR10"],
+}
 
 
-def write(request, key):
-    bucket = _get_or_create_bucket()
-    trust_data = bucket.new(key, data=DATA)
+@require_POST
+def search_trust(request, trust_key):
+    # Get Bucket
+    trust_bucket = _get_or_create_trust_bucket()
+    region_bucket = _get_or_create_region_bucket()
+
+    # Get Region Code
+    region_code = None
+    for k, v in RegionTrustMap.iteritems():
+        if trust_key in v:
+            region_code = k
+    if not region_code:
+        return HttpResponse("Trust Specified Not In Any of 4 Regions")
+
+    # Get Region Dict
+    region_dict = _get_region(region_bucket)
+
+    # Get Trust Data
+    trust_dict = _get_trust(trust_bucket, trust_key)
+
+    # Form Resp Dict
+    resp_data = {
+        "RegionCode": region_code,
+        "RegionData": region_dict,
+        "TrustData": trust_dict,
+    }
+    return JsonResponse(resp_data, safe=False)
+
+
+def _get_trust(trust_bucket, trust_key):
+    trust_dict = {}
+    trust_obj = trust_bucket.get(trust_key)
+    trust_dict[trust_key] = trust_obj.data
+    # TODO :- Get Trust Peers
+    return trust_dict
+
+
+def _get_region(region_bucket):
+    region_dict = {}
+    for region_key in region_bucket.get_keys():
+        region_obj = region_bucket.get(region_key)
+        region_dict[region_key] = region_obj.data
+    return region_dict
+
+
+@require_POST
+def write_trust(request, trust_key):
+    bucket = _get_or_create_trust_bucket()
+    trust_data = bucket.new(trust_key, data=TrustData)
     trust_data.store()
-    return HttpResponse("Write Success")
+    return HttpResponse("Trust Write Success")
 
 
-def _get_or_create_bucket():
-    return riak_crud.riak_client.bucket(riak_crud.bucket_name)
+@require_POST
+def delete_trust(request, trust_key):
+    bucket = _get_or_create_trust_bucket()
+    bucket.delete(trust_key)
+    return HttpResponse("Trust Delete Success")
 
 
-def delete(request, key):
-    bucket = riak_crud.riak_client.bucket(riak_crud.bucket_name)
-    bucket.delete(key)
-    return HttpResponse("Delete Success")
+@require_POST
+def update_trust(reqeust, trust_key):
+    # TODO Add Update Functionality
+    return HttpResponse("Trust Update Success")
 
 
-def update(reqeust, key):
-    return HttpResponse("Update Success")
+@require_POST
+def write_region(request, region_key):
+    region_bucket = _get_or_create_region_bucket()
+    region_data = region_bucket.new(region_key, data=RegionData)
+    region_data.store()
+    return HttpResponse("Region Write Success")
+
+
+@require_POST
+def delete_region(request, region_key):
+    region_bucket = _get_or_create_region_bucket()
+    region_bucket.delete(region_key)
+    return HttpResponse("Region Delete Success")
+
+
+@require_POST
+def update_region(request, region_key):
+    # TODO Add Update Functionality
+    return HttpResponse("Region Update Success")
+
+
+def _get_or_create_trust_bucket():
+    return riak_crud.riak_client.bucket(riak_crud.trust_bucket_name)
+
+
+def _get_or_create_region_bucket():
+    return riak_crud.riak_client.bucket(riak_crud.region_bucket_name)
