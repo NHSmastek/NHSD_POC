fffe3072304af4d702f84bcf9cbcfdbe3f45d62d
Usha Shinde <usha13529@Mastek.com>
2019-08-14 12:40:37 +0530

    project with dummy view added

---
+++ NHS.Ui/Web/PatientPathwayTimeline/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PatientpathwaytimelineConfig(AppConfig):
+    name = 'PatientPathwayTimeline'
--- NHS.Ui/Web/Web/settings.py
+++ NHS.Ui/Web/Web/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'Web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS':  [os.path.join(BASE_DIR, 'PatientPathwayTimeline/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
