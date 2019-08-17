4e379f177cb29614a958679d314d50153abfd7be
Akshay Deshpande <Akshay14469@Mastek.Com>
2019-08-17 15:35:15 +0530

    retriving trust data of selected drop down

--- NHS.Ui/Web/NHSD_POC/static/NHSD_POC/trust_performance.js
+++ NHS.Ui/Web/NHSD_POC/static/NHSD_POC/trust_performance.js
@@ -1,6 +1,32 @@
-console.log("HELLO WORLD")
+function get_performance_data_for_map(trust_code) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            // Will arrange the data according to the map
+            console.log( JSON.parse(this.responseText))
+        }
+    };
+    xhttp.open("GET", trust_code, true);
+    xhttp.send();
+}
 
-function get_trust(trust_obj) {
-  var trust = trust_obj.value;
-  console.log(trust)
+window.onload = function() {
+    get_trust_list()
+}
+
+function get_trust_list() {
+    // Will get the trust_dict through AJAX call
+    var trust_list = [
+        {id:"RR8", value:"RR8"},
+        {id:"RR1", value:"RR1"},
+        {id:"RR2", value:"RR2"},
+        {id:"RR3", value:"RR3"},
+    ]
+    var select = document.getElementById("trust_list")
+    trust_list.forEach(function(item) {
+        var el = document.createElement("option");
+        el.textContent = item.id;
+        el.value = item.value;
+        select.appendChild(el)
+    });
 }
--- NHS.Ui/Web/NHSD_POC/urls.py
+++ NHS.Ui/Web/NHSD_POC/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views as nhsd_poc
 
 urlpatterns = [
-    path('', nhsd_poc.get_trust_list, name='TrustList'),
     path('PatientPathwayTimeline', nhsd_poc.patient_pathway_timeline, name='PatientPathwayTimeline'),
+    path('<str:trust_name>', nhsd_poc.get_trust_list, name='TrustList'),
 ]
