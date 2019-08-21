12cbd3794943c78570f1c7e006cf06cd2713f414
Akshay Deshpande <Akshay14469@Mastek.Com>
2019-08-21 14:38:38 +0530

    Upgraded django and python version for riak

--- NHS.Api/Python3.Riak.Ubuntu/NHS_Analytics/urls.py
+++ NHS.Api/Python3.Riak.Ubuntu/NHS_Analytics/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("DB_crud.urls")),
+    path('', include("NHS_AnalyticsAPI.urls")),
 ]
