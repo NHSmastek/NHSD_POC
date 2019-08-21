f478583043fb139b9bf5dbd7296079f8ae450e56
Jayvant Butere <Jayvant14359@Mastek.Com>
2019-08-21 14:21:12 +0530

    login authentication

--- NHS.Ui/Web/NHSD_POC/views.py
+++ NHS.Ui/Web/NHSD_POC/views.py
@@ -3,7 +3,7 @@ from django.http import JsonResponse
 from django.shortcuts import render
 import requests
 from django.conf import settings
-
+from django.contrib.auth.decorators import login_required
 
 def get_trust_list(request, trust_name):
     url = "{}:{}/search_trust/{}".format(settings.NHS_API_DOMAIN_NAME, settings.NHS_API_DOMAIN_PORT, trust_name)
@@ -12,6 +12,6 @@ def get_trust_list(request, trust_name):
     trust_performance = trust_performance_obj.json()
     return JsonResponse(trust_performance)
 
-
+@login_required
 def patient_pathway_timeline(request):
     return render(request, "NHSD_POC/trust_performance.html", {})
--- NHS.Ui/Web/Web/settings.py
+++ NHS.Ui/Web/Web/settings.py
@@ -121,7 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+LOGIN_REDIRECT_URL = "/NHSD_POC"
+LOGOUT_REDIRECT_URL = "/login"
+LOGIN_URL = "/login"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/NHSD_POC'),
 ]
