077fb723cd3680ae4d638980b931d00c97ef039b
Akshay Deshpande <Akshay14469@Mastek.Com>
2019-08-21 14:44:40 +0530

    Upgraded django and python version for riak

--- NHS.Api/Python3.Riak.Ubuntu/NHS_AnalyticsAPI/urls.py
+++ NHS.Api/Python3.Riak.Ubuntu/NHS_AnalyticsAPI/urls.py
@@ -3,7 +3,7 @@ from . import views as riak_views
 
 urlpatterns = [
     # CRUD for Trust
-    path("^search_trust/<str:trust_name>", riak_views.search_trust, name="search_trust"),
+    path("search_trust/<str:trust_name>", riak_views.search_trust, name="search_trust"),
     path("get_trust_list", riak_views.get_trust_list, name="trust_list"),
     # url("^write_trust", riak_views.write_trust, name="write_trust"),
     # url("^update_trust", riak_views.update_trust, name="update_trust"),
