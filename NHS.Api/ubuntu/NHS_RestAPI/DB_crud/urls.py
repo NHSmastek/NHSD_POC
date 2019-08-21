d4d5064db7b4674bd4f8b6b71077f820b46999db
Akshay Deshpande <Akshay14469@Mastek.Com>
2019-08-21 11:48:51 +0530

    Updated django and python version

--- NHS.Api/ubuntu/NHS_RestAPI/DB_crud/urls.py
+++ NHS.Api/ubuntu/NHS_RestAPI/DB_crud/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views as riak_views
 
 urlpatterns = [
     # CRUD for Trust
-    url("^search_trust/<str:trust_name>", riak_views.search_trust, name="search_trust"),
-    url("get_trust_list/", riak_views.get_trust_list, name="trust_list"),
+    path("^search_trust/<str:trust_name>", riak_views.search_trust, name="search_trust"),
+    path("get_trust_list", riak_views.get_trust_list, name="trust_list"),
     # url("^write_trust", riak_views.write_trust, name="write_trust"),
     # url("^update_trust", riak_views.update_trust, name="update_trust"),
     # url("^delete_trust", riak_views.delete_trust, name="delete_trust"),
